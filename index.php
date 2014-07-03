7616c1665e057e414379c5e84e4e41ca9567f6a8
trigdzeen <trigdzeen@gmail.com>
2014-07-03 13:55:01 +0200

    homepage

---
+++ index.php
@@ -0,0 +1,170 @@
+<html><head>
+    <meta charset="utf-8">
+    <title>
+            
+             Tshewang Rinzin</title>
+    <meta name="author" content="iltaen">
+    <meta name="keywords" content="creative, modern, minimal, elegant, clear, parallax, clean, dark, circle, portfolio, personal, orange, photo">
+    <meta name="description" content="Creative portfolio HTML template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <meta http-equiv="X-UA-Compatible" content="edge">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="assets/font-awesome-4.1.0/css/font-awesome.css">
+    
+
+    <link rel="stylesheet" type="text/css" href="assets/css/basic.css">
+    <link rel="stylesheet" type="text/css" media="only screen and (max-width: 1024px)" href="assets/css/mobile.css">
+
+
+    <!--JavaScript-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="assets/js/masonry.pkgd.min.js"></script>
+    <script src="assets/js/script.js" type="text/javascript"></script>
+
+
+    <style>
+       #overlay{
+           position: fixed;
+           z-index: 1000;
+           left: 0;
+           top: 100px;
+           width: 25px;
+           height: 20px;
+           border-radius: 0 20px 20px 0;
+           padding: 5px;
+           background: rgba(242, 101, 34, 0.8);
+           overflow: hidden;
+
+           -webkit-transition: all 0.5s ease;
+           -moz-transition: all 0.5s ease;
+           -ms-transition: all 0.5s ease;
+           -o-transition: all 0.5s ease;
+           transition: all 0.5s ease;
+       }
+       #overlay:hover{
+           width: 200px;
+       }
+
+       #overlay a{
+           text-shadow: none;
+           margin-left: 5px;
+           font-size: 14px;
+           width: 100%;
+           float: left;
+           color: #fff;
+       }
+    </style>
+</head>
+
+<body>
+  <div id="background" style="zoom: 1;"></div>
+ <!-- <div id="texture" style="zoom: 1;"></div>-->
+
+ <!-- <div id="overlay">
+      <a href="#" class="additem"> <i class="icon-plus"></i> Add menu item</a>
+  </div>
+  -->
+  <div class="fullcontent">
+      <div class="menuwrapper">
+
+        <div class="mainmenu">
+
+          <div class="menucenter">
+            <div class="logo-container">
+<!-- ==========  Logo ========== -->
+             <!-- <img class="logo" alt="logotype" src="assets/img/tr.jpg"> -->
+<!-- ==========  User's photo ========== -->
+              <a href=""><img class="photo" alt="photo" src="assets/img/me2.JPG"></a>
+            </div>
+                    <h3 class="mobile bcolor">
+                        <a href="">Tshewang</a>
+                    </h3>
+                    <div class="mobile">Web designer, developer</div>
+          </div>
+
+        <div class="menu-items">
+
+<!-- ========  Copy this block for add new menu item: ======== -->
+                  <!--Start block-->
+            <a href="pages/index.html" class="menuitem active">
+              <i class="fa fa-home"></i>
+            <div class="menu-title">Home</div>
+          </a>
+          <a href="pages/about.html" class="menuitem">
+            <i class="flaticon-user77"></i>
+            <div class="menu-title">About</div>
+          </a>
+                  <!--end-->
+
+         
+
+          <a href="pages/skills.html" class="menuitem">
+            <i class="flaticon-tasks"></i>
+            <div class="menu-title">Skills</div>
+          </a>
+
+          <a href="pages/portfolio.html" class="menuitem bigpage">
+            <i class="flaticon-briefcase11"></i>
+            <div class="menu-title">Portfolio</div>
+          </a>
+
+          <a href="pages/blog.html" class="menuitem bigpage">
+            <i class="flaticon-font2"></i>
+            <div class="menu-title">Blog</div>
+          </a>
+          
+           <a href="pages/contacts.html" class="menuitem">
+            <i class="flaticon-envelope4"></i>
+            <div class="menu-title">Contacts</div>
+          </a>
+
+          <a href="pages/external.html" class="menuitem">
+            <i class="flaticon-link15"></i>
+            <div class="menu-title">External link</div>
+          </a>
+
+        </div>
+
+
+              <div id="linecontainer" class="select-about" style="-webkit-transform: rotate(30deg);">
+                  <div class="linewrapper">
+                      <div class="overwrapper" style="-webkit-transform: rotate(120deg);">
+                          <div class="colorlines large">
+                              <div class="colorlines small"></div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+        <div class="page">
+
+<!-- ======== AJAX Loader message ======== -->
+            <div class="loader">
+                <div class="label glcolor">
+                    <div class="spin"></div>
+                    Loading, please wait...
+                </div>
+            </div>
+            <div class="wrapper active" style="height: 342px;">
+                <div class="table">
+                    <div class="table-cell">
+                        <div class="container">
+                          <h1>Hi! I'm <span class="glcolor">Tshewang</span></h1>
+<p> Welcome! I believe you are either looking for web developer or you already know me and wants to know further about me. I am a student of College of Science and Technology, Royal University of Bhutan studying B.E Information Technology, 4th year. Hopefully, I will be completing my degree by July 2015. Starting the 9th grade in High School, I have been very passionate about studying IT. Athough IT is a broad subject, I would like to say that I'm interested in everything regarding IT. I have been working with web development projects for last one and half year to increase and broaden my programming and designing skills.
+
+</p>
+</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+  </div>
+
+
+</body></html>
---
+++ pages/about.php
@@ -0,0 +1,171 @@
+<html><head>
+    <meta charset="utf-8">
+    <title>
+            
+            About | Tshewang Rinzin</title>
+    <meta name="author" content="iltaen">
+    <meta name="keywords" content="creative, modern, minimal, elegant, clear, parallax, clean, dark, circle, portfolio, personal, orange, photo">
+    <meta name="description" content="Creative portfolio HTML template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <meta http-equiv="X-UA-Compatible" content="edge">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome/flaticon.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/basic.css">
+    <link rel="stylesheet" type="text/css" media="only screen and (max-width: 1024px)" href="assets/css/mobile.css">
+
+
+    <!--JavaScript-->
+    <script async="" src="//www.google-analytics.com/analytics.js"></script><script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="assets/js/masonry.pkgd.min.js"></script>
+    <script src="assets/js/script.js" type="text/javascript"></script>
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-43489798-1', 'iltaen.com');
+        ga('send', 'pageview');
+    </script>
+
+    <style>
+       #overlay{
+           position: fixed;
+           z-index: 1000;
+           left: 0;
+           top: 100px;
+           width: 25px;
+           height: 20px;
+           border-radius: 0 20px 20px 0;
+           padding: 5px;
+           background: rgba(242, 101, 34, 0.8);
+           overflow: hidden;
+
+           -webkit-transition: all 0.5s ease;
+           -moz-transition: all 0.5s ease;
+           -ms-transition: all 0.5s ease;
+           -o-transition: all 0.5s ease;
+           transition: all 0.5s ease;
+       }
+       #overlay:hover{
+           width: 200px;
+       }
+
+       #overlay a{
+           text-shadow: none;
+           margin-left: 5px;
+           font-size: 14px;
+           width: 100%;
+           float: left;
+           color: #fff;
+       }
+    </style>
+</head>
+
+<body>
+  <div id="background" style="zoom: 1;"></div>
+  <div id="texture" style="zoom: 1;"></div>
+
+  <!--<div id="overlay">
+      <a href="#" class="additem"> <i class="icon-plus"></i> Add menu item</a>
+  </div>-->
+
+  <div class="fullcontent">
+      <div class="menuwrapper">
+
+        <div class="mainmenu">
+
+          <div class="menucenter">
+            <div class="logo-container">
+<!-- ==========  Logo ========== -->
+             <!-- <img class="logo" alt="logotype" src="assets/img/tr.jpg"> -->
+<!-- ==========  User's photo ========== -->
+              <img class="photo" alt="photo" src="assets/img/me2.jpg">
+            </div>
+                    <h3 class="mobile bcolor">
+                        <a href="">Tshewang</a>
+                    </h3>
+                    <div class="mobile">Web designer, developer</div>
+          </div>
+
+        <div class="menu-items">
+
+<!-- ========  Copy this block for add new menu item: ======== -->
+                  <!--Start block-->
+          <a href="pages/about.html" class="menuitem active" style="top: 220px; left: 374px;">
+            <i class="flaticon-user77"></i>
+            <div class="menu-title">About</div>
+          </a>
+                  <!--end-->
+
+          <a href="pages/contacts.html" class="menuitem" style="top: 353.3679121828036px; left: 297px;">
+            <i class="flaticon-envelope4"></i>
+            <div class="menu-title">Contacts</div>
+          </a>
+
+          <a href="pages/skills.html" class="menuitem" style="top: 353.3679121828036px; left: 143.00000000000006px;">
+            <i class="flaticon-tasks"></i>
+            <div class="menu-title">Skills</div>
+          </a>
+
+          <a href="pages/portfolio.html" class="menuitem bigpage" style="top: 220.00000000000003px; left: 66px;">
+            <i class="flaticon-briefcase11"></i>
+            <div class="menu-title">Portfolio</div>
+          </a>
+
+          <a href="pages/blog.html" class="menuitem" style="top: 86.63208781719649px; left: 142.99999999999991px;">
+            <i class="flaticon-font2"></i>
+            <div class="menu-title">Blog</div>
+          </a>
+
+          <a href="pages/external.html" class="menuitem" style="top: 86.63208781719638px; left: 296.9999999999999px;">
+            <i class="flaticon-star60"></i>
+            <div class="menu-title">External link</div>
+          </a>
+
+        </div>
+
+
+              <div id="linecontainer" class="select-about" style="-webkit-transform: rotate(30deg);">
+                  <div class="linewrapper">
+                      <div class="overwrapper" style="-webkit-transform: rotate(120deg);">
+                          <div class="colorlines large">
+                              <div class="colorlines small"></div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+        <div class="page">
+
+<!-- ======== AJAX Loader message ======== -->
+            <div class="loader">
+                <div class="label glcolor">
+                    <div class="spin"></div>
+                    Loading, please wait...
+                </div>
+            </div>
+
+            <div class="wrapper active" style="height: 342px;">
+                <div class="table">
+                    <div class="table-cell">
+                        <div class="container"><h1>Hi! I'm <span class="glcolor">Tshewang</span></h1>
+<p> Welcome! I believe you are either looking for web developer or you already know me and wants to know further about me. I am a student of College of Science and Technology, Royal University of Bhutan studying B.E Information Technology, 4th year. Hopefully, I will be completing my degree by July 2015. Starting the 9th grade in High School, I have been very passionate about studying IT. Athough IT is a broad subject, I would like to say that I'm interested in everything regarding IT. I have been working with web development projects for last one and half year to increase and broaden my programming and designing skills.
+
+</p>
+</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+  </div>
+
+
+</body></html>
