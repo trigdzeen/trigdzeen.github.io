6f59d5037b0f8323a6613e4bc600e4674ad2af0b
trigdzeen <trigdzeen@gmail.com>
2014-07-04 04:13:34 +0200

    updated email

--- models/sendmail.php
+++ models/sendmail.php
@@ -39,7 +39,7 @@ if (!isset($_POST["submit"])) {
       // message lines should not exceed 70 characters (PHP rule), so wrap it
       $message = wordwrap($message, 70);
       // send mail
-      mail("webmaster@example.com",$subject,$message,"From: $from\n");
+      mail("trigdzeen@gmail.com",$subject,$message,"From: $from\n");
       echo "Thank you for sending us feedback";
     }
   }
