286a9226764d312c21b4a32de52544793f483049
trigdzeen <trigdzeen@gmail.com>
2014-07-04 06:05:40 +0200

    mail

--- models/sendmail.php
+++ models/sendmail.php
@@ -1,49 +1,38 @@
-<html>
-<body>
 <?php
-function spamcheck($field) {
-  // Sanitize e-mail address
-  $field=filter_var($field, FILTER_SANITIZE_EMAIL);
-  // Validate e-mail address
-  if(filter_var($field, FILTER_VALIDATE_EMAIL)) {
-    return TRUE;
-  } else {
-    return FALSE;
-  }
+require 'PHPMailerAutoload.php';
+ 
+$mail = new PHPMailer;
+ 
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'amit@gmail.com';                   // SMTP username
+$mail->Password = 'digitalinspiration';               // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+$mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
+$mail->setFrom('trigdzeen@gmail.com', 'Tshewang Rinzin');     //Set who the message is to be sent from
+$mail->addReplyTo('trigdzeen@gmail.com', 'Tshewang Rinzin');  //Set an alternative reply-to address
+$mail->addAddress('trigdzeen@gmail.com', 'Tshewang Rinzin');  // Add a recipient
+$mail->addAddress('trigdzeen@yahoo.com');               // Name is optional
+$mail->addCC('cc@example.com');
+$mail->addBCC('bcc@example.com');
+$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+/*$mail->addAttachment('/usr/labnol/file.doc');         // Add attachments
+$mail->addAttachment('/images/image.jpg', 'new.jpg');*/ // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+ 
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+ 
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+ 
+if(!$mail->send()) {
+   echo 'Message could not be sent.';
+   echo 'Mailer Error: ' . $mail->ErrorInfo;
+   exit;
 }
-?>
-
-<h2>Feedback Form</h2>
-<?php
-// display form if user has not clicked submit
-if (!isset($_POST["submit"])) {
-  ?>
-  <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-  From: <input type="text" name="from"><br>
-  Subject: <input type="text" name="subject"><br>
-  Message: <textarea rows="10" cols="40" name="message"></textarea><br>
-  <input type="submit" name="submit" value="Submit Feedback">
-  </form>
-  <?php 
-} else {  // the user has submitted the form
-  // Check if the "from" input field is filled out
-  if (isset($_POST["from"])) {
-    // Check if "from" email address is valid
-    $mailcheck = spamcheck($_POST["from"]);
-    if ($mailcheck==FALSE) {
-      echo "Invalid input";
-    } else {
-      $from = $_POST["from"]; // sender
-      $subject = $_POST["subject"];
-      $message = $_POST["message"];
-      // message lines should not exceed 70 characters (PHP rule), so wrap it
-      $message = wordwrap($message, 70);
-      // send mail
-      mail("trigdzeen@gmail.com",$subject,$message,"From: $from\n");
-      echo "Thank you for sending us feedback";
-    }
-  }
-}
-?>
-</body>
-</html>
+ 
+echo 'Message has been sent';
